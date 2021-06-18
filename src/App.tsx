8e82587ffa80b3baf65c32f178016dd7382ebb53
Heath Daniel <mr.heath.daniel@gmail.com>
2021-06-18 13:22:09 -0400

    Hide notes list after opening note

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const initialState: State = {
 function App() {
   
   const [state, dispatch] = useReducer(reducer, initialState);
+
   const { notes, currentNoteIndex, isEditing, editorTitleText, editorBodyText } = state;
   
     useEffect(() => {
@@ -37,66 +38,71 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <h1>Notes App</h1>
-      </header>
+      {
+        currentNoteIndex === null ? (
+          <>
+          <header>
+            <h1>Notes App</h1>
+          </header>
 
-      <nav>
-        <h2>All Notes</h2>
-        <button onClick={() => dispatch({ type: 'CREATE_NOTE' })}>New</button>
-        {notes.length ? (
-        <ul>
-          {notes.map((note, i) => (
-            <li
-              onClick={() => dispatch({ type: 'OPEN_NOTE', payload: i })}
-              className={i === currentNoteIndex ? 'emphasis' : ''}
-            >
-              {note.title}
-            </li>
-          ))}
-        </ul>
-        ): (
-          // Empty State
-          <p>Add your first note!</p>
-        )}
-      </nav>
-      {currentNoteIndex !== null ? (
-        <div>
-          {isEditing ? (
-            <div className="vertical">
-              <div>
-                <button onClick={() => dispatch({type: 'CANCEL_EDIT'})}>Cancel</button>
-                <button onClick={() => dispatch({type: 'SAVE_EDIT'})}>Save</button>
-              </div>
-              <input value={editorTitleText} onChange={handleTitleChange} />
-              <textarea value={editorBodyText} onChange={handleBodyChange} />
-              <button
-                onClick={() => dispatch({type: 'DELETE_NOTE'})}
-              >
-                Delete
-              </button>
-            </div>
-          ) : (
-            <div>
-              <div>
-                <button
-                  onClick={() => dispatch({type: 'CLOSE_NOTE'})}
+          <nav>
+            <h2>All Notes</h2>
+            <button onClick={() => dispatch({ type: 'CREATE_NOTE' })}>New</button>
+            {notes.length ? (
+            <ul>
+              {notes.map((note, i) => (
+                <li
+                  onClick={() => dispatch({ type: 'OPEN_NOTE', payload: i })}
+                  className={i === currentNoteIndex ? 'emphasis' : ''}
                 >
-                  Back
-                </button>
+                  {note.title}
+                </li>
+              ))}
+            </ul>
+            ): (
+              // Empty State
+              <p>Add your first note!</p>
+            )}
+          </nav>
+          </>
+        ) : (
+          <div>
+            {isEditing ? (
+              <div className="vertical">
+                <div>
+                  <button onClick={() => dispatch({type: 'CANCEL_EDIT'})}>Cancel</button>
+                  <button onClick={() => dispatch({type: 'SAVE_EDIT'})}>Save</button>
+                </div>
+                <input value={editorTitleText} onChange={handleTitleChange} />
+                <textarea value={editorBodyText} onChange={handleBodyChange} />
                 <button
-                  onClick={() => dispatch({type: 'START_EDIT'})}
+                  onClick={() => dispatch({type: 'DELETE_NOTE'})}
                 >
-                  Edit
+                  Delete
                 </button>
               </div>
-              <h3>{notes[currentNoteIndex].title}</h3>
-              <div dangerouslySetInnerHTML={{ __html: marked(notes[currentNoteIndex].body)}}>
+            ) : (
+              <div>
+                <div>
+                  <button
+                    onClick={() => dispatch({type: 'CLOSE_NOTE'})}
+                  >
+                    Back
+                  </button>
+                  <button
+                    onClick={() => dispatch({type: 'START_EDIT'})}
+                  >
+                    Edit
+                  </button>
+                </div>
+                <h3>{notes[currentNoteIndex].title}</h3>
+                <div dangerouslySetInnerHTML={{ __html: marked(notes[currentNoteIndex].body)}}>
+                </div>
               </div>
-            </div>
-          )}
-        </div>
-      ) : null}
+            )}
+          </div>
+        )
+      }
     </div>
   );
 }
